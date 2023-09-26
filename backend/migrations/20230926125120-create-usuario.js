5d38dc2d47cc348cb32ffd4ff87c17bd62374aea
Evelin Jesus Dos Santos <00001202562358sp@aluno.educacao.sp.gov.br>
2023-09-26 11:06:52 -0300

    migrations

---
+++ backend/migrations/20230926125120-create-usuario.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Usuarios', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nomeDeUsuario: {
+        type: Sequelize.STRING
+      },
+      fotoDePerfil: {
+        type: Sequelize.STRING
+      },
+      dataDeNascimento: {
+        type: Sequelize.DATE
+      },
+      idGenero: {
+        type: Sequelize.INTEGER
+      },
+      biografia: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Usuarios');
+  }
+};
