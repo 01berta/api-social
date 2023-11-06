d2914f72cb21487028b038c9d98f94935cf43662
Rhuan Lopes Vieira <00001155499372sp@aluno.educacao.sp.gov.br>
2023-11-06 12:11:52 -0300

    controller de usuario

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -13,5 +13,33 @@ router.post('/add', async (req, res) => {
         res.status(500).json({ message: "error ao cadastrar", error });
     }
 });
+//Busca Usuario (GET)
+router.get('/all', async (req, res) => {
+const usuarios = await Usuario.findAll();
+res.status(200).json(usuarios);
+});
+//Altera Usuario por ID (PUT)
+router.put('/:id', async (req, res) =>{
+    const { nomeDeUsuario, fotoDePerfil, dataDeNascimento, biografia} = req.body;
+
+await Usuario.update(
+{ nomeDeUsuario, fotoDePerfil, dataDeNascimento, biografia},
+{
+    where: {id:req.params.id},
+}
+);
+res.status(200).json({message: 'Atualizado com sucesso'});
+});
+
+//deletar o usuario
+router.delete('/:id', async (req, res) =>{
+await Usuario.destroy({
+where:{
+    id: req.params.id,
+},
+});
+res.status(200).json({message:'Excluido com sucesso'})
+});
+
 
 module.exports = router;
