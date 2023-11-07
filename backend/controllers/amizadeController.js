7fa230ccde26bcadb0a8bb87e58e24cbc23a12bd
Rhuan Lopes Vieira <00001155499372sp@aluno.educacao.sp.gov.br>
2023-11-07 11:34:11 -0300

    amizadeseguindo

--- backend/controllers/amizadeController.js
+++ backend/controllers/amizadeController.js
@@ -6,14 +6,29 @@ const AmizadeSeguindo = require('../models').AmizadeSeguindo;
 //Cadastra AmizadeSeguindo (POST)
 router.post('/add', async (req, res) => {
     try {
-        const {idSeguidor, idSeguido, dataDeinicio} = req.body;
-        const com = await AmizadeSeguindo.create({ idSeguidor, idSeguido, dataDeinicio})
+        const { idSeguidor, idSeguido, dataDeinicio } = req.body;
+        const com = await AmizadeSeguindo.create({ idSeguidor, idSeguido, dataDeinicio })
         res.status(200).json({ message: 'Cadastrado com sucesso', com });
     } catch (error) {
         res.status(500).json({ message: "error ao cadastrar", error });
     }
 });
 
-
+//Buscar todas as amizades do usuario (GET)
+router.get('/:id', async (req, res) => {
+    const { id } = req.params; // Este é o ID do seguidor
+    try {
+        const amizade = await AmizadeSeguindo.sequelize.query(
+            'SELECT * FROM amizade_seguido WHERE idSeguidor = :id',
+            {
+                type: AmizadeSeguindo.sequelize.QueryTypes.SELECT,
+                replacements: { id },
+            }
+        );
+        res.status(200).json(amizade);
+    } catch (error) {
+        res.status(500).json({ message: "error ao cadastrar", error });
+    }
+});
 
 module.exports = router;
